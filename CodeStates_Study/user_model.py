a99d85f5742d625661c081a1121ffedb54eadb11
Sohee Han <79372217+mynameis-sohee@users.noreply.github.com>
2021-04-09 15:27:37 +0900

    Delete user_model.py

--- CodeStates_Study/user_model.py
+++
@@ -1,7 +0,0 @@
-from train_flask import db
-
-class User(db.Model): #=>Base
-    __tablename__ = 'user'		
-    __table_args__ = {'extend_existing': True}
-    id=db.Column(db.String(32), primary_key=True) #SQLALchemy에서 굳이 여러개 갖고 올 필요가 없다.
-    username=db.Column(db.String(32))
