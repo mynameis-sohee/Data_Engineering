08000fdcee141276702fd8c3d3a110e06f0cd7ed
Sohee Han <79372217+mynameis-sohee@users.noreply.github.com>
2021-04-09 15:34:04 +0900

    03. One-to-Many관계 & ForeignKey
    폴더변경

--- FLASK/03. One-to-Many관계 & ForeignKey.py
+++ FLASK/03. One-to-Many관계 & ForeignKey.py
@@ -1,5 +1,4 @@
-# one-to-many 관계, User의 role_id는 Role의 id이다.
-
+# one-to-many 관계, User의 role_id는 Role의 id.
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -12,7 +11,8 @@ db = SQLAlchemy(app)
 class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     position = db.Column(db.String(50))
-    users = db.relationship('User',backref='role',lazy='dynamic') #위 외래키연결을 위해 작성. backref: 테이블명(전체 소문자), lazy: 받는 방식 
+    #위 외래키연결을 위해 작성. backref: 테이블명(전체 소문자), lazy: 받는 방식 
+    users = db.relationship('User',backref='role',lazy='dynamic')
     
     def __repr__(self):
         return '<Role %r>' % self.position
@@ -23,7 +23,8 @@ class User(db.Model):
     __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), nullable=False)
-    role_id = db.Column(db.Integer, db.ForeignKey('role.id')) #Role의 id를 외래키로 가져오겠다는 의미(테이블은 전부 소문자로 인식)
+    #Role의 id를 외래키로 가져오겠다는 의미(테이블은 전부 소문자로 인식)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id')) 
     
     def __repr__(self):
         return "{self.id}, {self.username}"
